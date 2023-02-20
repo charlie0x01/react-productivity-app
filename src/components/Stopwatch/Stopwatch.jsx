4fd4e08c171e1b97e942beb4c2aecc9a13ab34d1
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-02-20 16:46:01 +0500

    stopwatch working as expected

--- src/components/Stopwatch/Stopwatch.jsx
+++ src/components/Stopwatch/Stopwatch.jsx
@@ -1,34 +1,59 @@
 import React, { useState, useEffect } from "react";
 import "./Stopwatch.css";
 
-const Stopwatch = () => {
+const Stopwatch = ({ endTime }) => {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [stopped, setStopped] = useState(true);
+
+
+  const getTime = () => {
+    let sec = ("0" + Math.floor(time)).slice(-2)
+    let mins0 = ("0" + Math.floor(time)).slice(-2)
+    let hrs = ("0" + Math.floor(time)).slice(-2)
+  }
+
   useEffect(() => {
     let interval;
     if (running) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 1);
+        if (time === 59) {
+          setTime(0);
+          setMinutes(minutes + 1);
+        }
+        if (minutes === 59) {
+          setMinutes(0);
+          setHours(hours + 1);
+        }
       }, 1000);
-    } else if (!running && time === 0) {
+    } else if (!running) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
   }, [running, time]);
   return (
     <div className="stopwatch">
-      {time === 0 ? (
-        <button onClick={() => setRunning(true)} className="">
+      {stopped === true ? (
+        <button
+          onClick={() => {
+            setRunning(true);
+            setStopped(false);
+          }}
+          className="button is-small"
+        >
           <i className="fa-solid fa-play"></i>
         </button>
       ) : (
-        <div className="d-flex flex-row">
+        <div className="">
           {running === true ? (
-            <button onClick={() => setRunning(false)} className="">
+            <button onClick={() => setRunning(false)} className="button is-small">
               <i className="fa-solid fa-pause"></i>
             </button>
           ) : (
-            <button onClick={() => setRunning(true)} className="">
+            <button onClick={() => setRunning(true)} className="button is-small">
               <i className="fa-solid fa-play"></i>
             </button>
           )}
@@ -36,19 +61,35 @@ const Stopwatch = () => {
           <button
             onClick={() => {
               setRunning(false);
+              setStopped(true);
+              setHours(0);
+              setMinutes(0);
               setTime(0);
             }}
-            className="ms-1"
+            className="ms-1 button is-small"
           >
             <i className="fa-solid fa-stop"></i>
           </button>
         </div>
       )}
-      {time !== 0 ? (
-        <div className="ms-2">
-          {/* <span>{("0" + Math.floor(time / 3600)).slice(-2)}:</span> */}
-          <span>{("0" + Math.floor(time / 60)).slice(-2)}:</span>
-          <span>{("0" + Math.floor(time)).slice(-2)}</span>
+
+      {stopped === false ? (
+        <div className="timer">
+          {hours > 0 ? (
+            <span>{("0" + Math.floor(hours)).slice(-2)}:</span>
+          ) : (
+            <></>
+          )}
+          {minutes > 0 ? (
+            <span>{("0" + Math.floor(minutes)).slice(-2)}:</span>
+          ) : (
+            <></>
+          )}
+          {time > 0 ? (
+            <span>{("0" + Math.floor(time)).slice(-2) + "s"}</span>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
         <></>
