64291d000504480aceaadf7e189045f0f8dd68f3
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-02-21 18:31:15 +0500

    del: removed unnecessary files to start again with mdb-ui-kit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import Stopwatch from "./components/Stopwatch/Stopwatch";
-// import Stopwatch from "./components/Stopwatch/Stopwatch";
 import "./index.css";
 
+const list = [1, 2, 3, 4, 5];
 function App() {
   return (
     <>
-    <div className="box is-flex is-justify-content-center">
-      <Stopwatch endTime={{ seconds: 9, minutes: 0, hours: 0}} startTime={{seconds: 5, minutes: 0, hours: 0}}/>
-    </div>
+     <h1>Hello World!!</h1>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "bulma/css/bulma.min.css";
 
-import App from './App';
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
